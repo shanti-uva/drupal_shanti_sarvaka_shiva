5e4904eb9aa309f8ec4032f820bbe6f38cf97871
ThanGrove <than@virginia.edu>
2015-02-19 15:16:42 -0500

    update to display of share links

---
+++ templates/page.tpl.php
@@ -0,0 +1,15 @@
+
+<?php
+ /**
+  * This template is used by the share popup and any other light box popups to display just content without header and footer
+  * if ?format=simple, this will be activated
+  * otherwise it uses the default page template from shanti sarvaka
+  * 
+  **/
+	if(isset($_GET['format']) && $_GET['format'] == 'simple') {
+		print render($variables['page']['content']); 
+		return;
+	} else {
+		include_once(drupal_get_path('theme', 'shanti_sarvaka') . '/templates/page.tpl.php');
+	} ?>
+?>

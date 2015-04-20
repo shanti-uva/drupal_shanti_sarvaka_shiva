1e96e4dc2960e1b533e908c32dd617eb2e625da4
ThanGrove <than@virginia.edu>
2015-04-20 10:57:02 -0400

    update to shiva styling

--- template.php
+++ template.php
@@ -11,6 +11,33 @@
   */ 
   
 /**
+ * Implements hook_preprocess_page:
+ */
+ 
+function sarvaka_shiva_preprocess_page(&$vars) {
+	$vids = array();
+	$content = $vars['page']['content'];
+	// Find the all_vis and my_vis blocks and separate them out from the content into their own variables
+	foreach($content as $key => $view) {
+		if(strpos($key, 'views_') > -1) {
+			if(isset($view['#views_contextual_links_info']['views_ui']['view_display_id'])) {
+				if($view['#views_contextual_links_info']['views_ui']['view_display_id'] == 'all_vis_block') {
+					$vids['allvis'] = $key;
+				} else if($view['#views_contextual_links_info']['views_ui']['view_display_id'] == 'my_vis_block') {
+					$vids['myvis'] = $key;
+				}
+			}
+		}
+	}
+	if(count($vids) > 0) {
+		foreach($vids as $name => $vid) { 
+			$vars['page'][$name] = $vars['page']['content'][$vid];
+			unset($vars['page']['content'][$vid]);
+		}
+	}
+}
+
+/**
  * Implements hook_preprocess_node: adds variable "can_edit" to determine whether to display an edit button
  */
 function sarvaka_shiva_preprocess_node(&$vars) {
---
+++ templates/page--front.tpl.php
@@ -0,0 +1,169 @@
+<div class="wrap-all">
+   <span class="sr-only"><a href=".main-content">Skip to main content</a> <a href="#search-flyout">Skip to search</a></span>
+    <!-- Header Region -->
+   <header class="header-banner">
+    <div class="navbar navbar-default">
+
+	      <nav class="navbar-buttons" role="navigation">
+	        <span class="menu-icon menu-toggle"><a href="#"><span class="sr-only">Main Menu</span><span class="icon shanticon-menu"></span></a></span><!-- desktop > 768 drilldown menu : main-menu -->
+	        <span class="menu-explore menu-exploretoggle"><a href="#"><span>Explore</span><span class="icon shanticon-directions"></span></a></span><!-- mobile < 768 : collections -->
+	      </nav>
+
+	      <h1 class="navbar-header<?php if(!$variables['shanti_site']) { print " default"; } ?>">
+	        <a href="<?php print $variables['home_url']; ?>" class="navbar-brand" title="<?php print $site_name; ?> Homepage">
+	          <?php if($variables['shanti_site']): ?>
+	            <span class="icon shanticon-logo"></span><span class="shanti">SHANTI</span><?php if($variables['use_admin_site_title']) {
+		            	print "<span class=\"site-title\">{$site_name}</span>";
+		            } ?>
+	          <?php else: ?>
+	            <img src="<?php print $logo; ?>" class="site-logo" /> <span class="site-title"><?php print $site_name; ?></span>
+	          <?php endif; ?>
+	          <?php if($site_slogan) { print '<span class="site-slogan">' . $site_slogan . '</span>' ;} ?>
+	        </a>
+	      </h1>
+
+	      <!-- HEADER REGION -->
+	      <nav id="sarvaka-header" class="region navbar-collapse collapse navtop" role="navigation"> <!-- desktop display > 768 -->
+	         <form class="form">
+	         <fieldset>
+	          <ul class="nav navbar-nav navbar-right">
+		            <!-- If admin puts blocks in  header, render here -->
+		            <?php print render($page['header']);  ?>
+	          </ul>
+	         </fieldset>
+	         </form>
+	       </nav>
+	       <!-- End of HEADER region -->
+     </div>
+     <!-- include shanti-explore-menu if it exists -->
+     <?php if(module_exists('explore_menu')) { print render($variables['explore_menu']); } ?>
+    </header>
+
+
+    <!-- Begin Content Region -->
+    <main class="main-wrapper container-fluid">
+      <article class="main-content" role="main">
+        <div class="row">
+
+          <!-- Banner Region -->
+          <header class="col-xs-12 titlearea banner<?php print $variables['banner_class']; ?>">
+           <div role="banner">
+            <h1 class="page-title"><span class="icon shanticon-<?php print $variables['icon_class']; ?>"></span><span class="page-title-text">
+            <?php
+            	if(!empty($variables['default_title']) && !empty($variables['prefix_default_title'])) {
+            		print ($title == '')? $variables['default_title'] : $variables['default_title'] . ': ' . $title;
+            	} else {
+            		print ($title == '')? $variables['default_title']:$title;
+            	}
+              ?></span></h1>
+              <nav class="breadwrap" role="navigation" style="display:none;">
+                <?php print theme('breadcrumb', array('breadcrumb' => $breadcrumb)); ?>
+              </nav>
+              <div class="banner-content">
+                <?php print render($page['banner']); ?>
+              </div>
+              <div class="banner-tabs">
+                <?php
+                  // For view/edit tabs
+                  print render($tabs);
+                ?>
+              </div>
+            </div>
+          </header>
+
+        </div> <!-- End of Banner Row -->
+
+
+        <!-- Begin Content Row -->
+        <div class="row row-offcanvas row-offcanvas-left">
+
+          <!-- Sidebar First Region -->
+          <?php if ($page['sidebar_first']): ?>
+            <div id="sidebar-first" class="region sidebar<?php print " $bsclass_sb1"; ?>">
+              <?php print render($page['sidebar_first']); ?>
+            </div>
+          <?php endif; ?>
+
+          <!-- Begin Page Content -->
+          <section class="content-section col-xs-12<?php if (!empty($bsclass_main)) { print " $bsclass_main"; } ?>">
+	        
+		        <button type="button" class="btn btn-default view-resources" data-toggle="offcanvas" style="display:none;">
+	            <span class="icon"></span>
+	          </button>
+          
+          	<!-- Message Area -->
+          	<?php if (!empty($messages)) { print "<div class=\"messages\">$messages</div>"; } ?>
+          	
+          	<!-- Main Content -->
+            <div class="tab-content">
+              <article class="tab-pane main-col active" id="tab-overview">
+              	 <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+                 <?php print render($page['content']); ?>
+              </article>
+              <!-- All versus My Visualization Tabs -->
+              <div>
+				        <ul class="nav nav-tabs nav-justified" role="tablist">
+				          <li class="active"><a href="#allvis" role="tab" data-toggle="tab" id="allvis-tab"><?php print t('All Visualization'); ?></a></li>
+				          <li><a href="#myvis" role="tab" data-toggle="tab" id="myvis-tab"><?php print t('My Visualizations'); ?></a></li>
+				        </ul>
+				        <!-- Tab panes -->
+				        <div class="tab-content">
+				          <div class="tab-pane active" id="allvis">
+				          	<?php print $page['allvis']['#markup']; ?>
+				          </div>
+				          <div class="tab-pane" id="myvis">
+				          	<?php print $page['myvis']['#markup']; ?>
+				          </div>
+				        </div>
+				      </div>
+            </div>
+          </section>
+          <!-- END Content -->
+
+          <!-- Sidebar Second Region -->
+          <?php if ($page['sidebar_second']): ?>
+            <div id="sidebar-second" class="region sidebar pull-right sidebar-offcanvas<?php print " $bsclass_sb2"; ?>">
+              <?php print render($page['sidebar_second']); ?>
+            </div>
+          <?php endif; ?>
+        </div>
+
+        <a href="#" class="back-to-top"><span class="icon fa"></span></a>
+      </article>
+
+		  <!-- Search Flyout -->
+		  <?php if(!empty($page['search_flyout'])): ?>
+		      <!--print render($page['search_flyout']); -->
+				<div id="search-flyout" class="region extruder right" role="search" style="display: none;">
+				   <?php print render($page['search_flyout']); ?>
+				</div>
+	    <?php endif; ?>
+
+    </main> <!-- End Main Content -->
+
+
+  <!-- LOAD menus -->
+  <section id="menu" class="menu-main-desk" style="display:none;">
+    <nav id="menu-drill" role="navigation">
+     <?php print $variables['user_menu_links']; ?>
+    </nav>
+  </section><!-- END menu -->
+  
+</div> <!-- End wrap-all -->
+
+<!-- Footer -->
+<footer class="footer">
+  <div>
+    <p>&copy; Copyright 2015</p>
+    <?php print render($page['footer']); ?>
+  </div>
+</footer>
+
+<!-- Admin Footer -->
+<div id="admin-footer">
+  <?php print render($page['admin_footer']); ?>
+</div>
+
+
+
+

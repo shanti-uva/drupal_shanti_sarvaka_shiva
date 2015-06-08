52cd2d70d07db25a45debf1b390e4bb601d44dd6
mferrara <mf8yk@virginia.edu>
2015-06-08 18:42:11 -0500

    tests grid selector in js

--- js/shanti-main-shiva.js
+++ js/shanti-main-shiva.js
@@ -52,7 +52,7 @@
 		          	align: 'left',
 		            itemWidth: 160, // Optional min width of a grid item
 		            autoResize: true, // This will auto-update the layout when the browser window is resized.
-		            container: $('.shanti-gallery'), // Optional, used for some extra CSS styling
+		            container: $('.sn-explore-list'), // Optional, used for some extra CSS styling
 		            offset: 15, // Optional, the distance between grid items
 		            outerOffset: 0, // Optional the distance from grid to parent
 		            flexibleWidth: '30%', // Optional, the maximum width of a grid item
